74f5c92851fd7d898b88993409e2b5ff6fc07f12
Ali <ali.hamid9119@gmail.com>
2022-11-22 17:45:42 +0100

    Projektionsmatrix allgemeiner gehalten.

--- Projekt3/Projektionsmatrix.java
+++ Projekt3/Projektionsmatrix.java
@@ -9,12 +9,15 @@ import static Projekt3.Constants.WINDOW_WIDTH;
 public class Projektionsmatrix{
 
     public static void main(String[] args) {
-        double d = 90;
-        double a = Math.toRadians(d);
+        //Degree
+        double a = 90;
+        System.out.println(Math.cos(Math.toRadians(a)));
+
         float s1 = 1F/2F;
+
         float[][] Base = new float[2][4];
-        Base[0][0] = (float) (-s1 * Math.sin(a));
-        Base[1][0] = (float) (-s1 * Math.cos(a));
+        Base[0][0] = (float) (-s1 * Math.sin(Math.toRadians(a)));
+        Base[1][0] = (float) (-s1 * Math.cos(Math.toRadians(a)));
         Base[0][1] = 1;
         Base[1][1] = 0;
         Base[0][2] = 0;
